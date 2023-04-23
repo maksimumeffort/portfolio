c3997d222b86b3d91bc0e0fce0e0037d4b643c8f
mooreEffort <alexmaksimets@gmail.com>
2023-04-23 23:03:03 +1000

    complete about section

--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -1,9 +1,11 @@
+import { AboutSection } from "../AboutSection/AboutSection";
 import { ProjectsList } from "../ProjectsList/ProjectsList";
 import "./Body.modules.scss";
 
 export const Body = () => {
   return (
     <div div className="Body">
+      <AboutSection />
       <ProjectsList />
     </div>
   );
