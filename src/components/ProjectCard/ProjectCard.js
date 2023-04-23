e2a3c3fd6914826ca101faa374002f81fca89f86
mooreEffort <alexmaksimets@gmail.com>
2023-04-24 00:47:15 +1000

    fix images not showing on project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,15 +1,18 @@
 import "./ProjectCard.modules.scss";
+import { ReactComponent as Github } from "../../assets/icons/github.svg";
 
 export const ProjectCard = ({ project }) => {
   return (
-    <div div className="ProjectCard">
-      <img src={project.imgLink} alt="" class="img" />
-      <div class="bottom_section">
+    <div div className="card">
+      <img src={project.imgLink} alt="" class="card__img" />
+      <div class="card__bottom">
         <h3>{project.name}</h3>
         <p>{project.desc}</p>
-        <p>{project.stack}</p>
+        <p>Built with: {project.stack}</p>
         <div>
-          <a href={project.repo}>Repo</a>
+          <a href={project.repo}>
+            <Github />
+          </a>
         </div>
       </div>
     </div>
--- src/containers/ProjectsList/ProjectsList.js
+++ src/containers/ProjectsList/ProjectsList.js
@@ -1,32 +1,36 @@
 import "./ProjectsList.modules.scss";
 import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
 // images
+import employeeApi from "../../assets/images/employee_api.png";
+import bookSearch from "../../assets/images/book_search.png";
+import morseCode from "../../assets/images/morse_code.png";
+import eStore from "../../assets/images/eStore.png";
 
 export const ProjectsList = () => {
   const projects = [
     {
-      imgLink: "../../assets/images/book_search.png",
+      imgLink: employeeApi,
       name: "Employee API",
       desc: "Employee Database management application",
       stack: "HTML, SCSS, TypeScript, React, MySQL",
       repo: "https://github.com/maksimumeffort/book-search-react",
     },
     {
-      imgLink: "../../assets/images/book_search.png",
+      imgLink: bookSearch,
       name: "Book Search",
       desc: "Single page application search engine using Google Books API",
       stack: "HTML, SCSS, JavaScript, React",
       repo: "https://github.com/maksimumeffort/book-search-react",
     },
     {
-      imgLink: "../../assets/images/morse_code",
+      imgLink: morseCode,
       name: "Morse Code",
       desc: "Simple morse code translator",
       stack: "HTML, CSS, JavaScript",
       repo: "https://github.com/maksimumeffort/morse-translate",
     },
     {
-      imgLink: "../../assets/images/eStore.png",
+      imgLink: eStore,
       name: "eCommerce Store",
       desc: "Web app with FireStore databse",
       stack: "HTML, SCSS, JavaScript, React, Firebase",
