3d388c9943a56d22eeac7c026d53f102bbf23466
mooreEffort <alexmaksimets@gmail.com>
2023-04-24 00:18:47 +1000

    add project data to project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,9 +1,17 @@
 import "./ProjectCard.modules.scss";
 
-export const ProjectCard = () => {
+export const ProjectCard = ({ project }) => {
   return (
     <div div className="ProjectCard">
-      ProjectCard
+      <img src={project.imgLink} alt="" class="img" />
+      <div class="bottom_section">
+        <h3>{project.name}</h3>
+        <p>{project.desc}</p>
+        <p>{project.stack}</p>
+        <div>
+          <a href={project.repo}>Repo</a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/containers/ProjectsList/ProjectsList.js
+++ src/containers/ProjectsList/ProjectsList.js
@@ -1,13 +1,43 @@
 import "./ProjectsList.modules.scss";
 import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
+// images
 
 export const ProjectsList = () => {
-  const projects = ["BookSeach", "MorseCode", "E-Commerce", "Employee API"];
+  const projects = [
+    {
+      imgLink: "../../assets/images/book_search.png",
+      name: "Employee API",
+      desc: "Employee Database management application",
+      stack: "HTML, SCSS, TypeScript, React, MySQL",
+      repo: "https://github.com/maksimumeffort/book-search-react",
+    },
+    {
+      imgLink: "../../assets/images/book_search.png",
+      name: "Book Search",
+      desc: "Single page application search engine using Google Books API",
+      stack: "HTML, SCSS, JavaScript, React",
+      repo: "https://github.com/maksimumeffort/book-search-react",
+    },
+    {
+      imgLink: "../../assets/images/morse_code",
+      name: "Morse Code",
+      desc: "Simple morse code translator",
+      stack: "HTML, CSS, JavaScript",
+      repo: "https://github.com/maksimumeffort/morse-translate",
+    },
+    {
+      imgLink: "../../assets/images/eStore.png",
+      name: "eCommerce Store",
+      desc: "Web app with FireStore databse",
+      stack: "HTML, SCSS, JavaScript, React, Firebase",
+      repo: "https://github.com/maksimumeffort/eStore",
+    },
+  ];
 
   return (
     <div div className="ProjectsList">
       {projects.map((project) => {
-        return <ProjectCard />;
+        return <ProjectCard project={project} />;
       })}
     </div>
   );
